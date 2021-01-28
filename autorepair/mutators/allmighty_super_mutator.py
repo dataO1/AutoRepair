37786be370d1c4789370789cd7dc52873d844df2
Daniel Tabellion <wellos94@web.de>
2021-01-28 16:04:28 +0100

    operator_mutator works

--- autorepair/mutators/allmighty_super_mutator.py
+++ autorepair/mutators/allmighty_super_mutator.py
@@ -7,6 +7,9 @@ import copy
 from copy import deepcopy
 from debuggingbook.Repairer import StatementMutator, ConditionMutator
 
+rand = Random()
+rand.seed(42)
+
 def get_all_mutation_sources(trees, tp=None):
     """Return all conditions from the AST (or AST list) `trees`.
     If `tp` is given, return only elements of that type."""
@@ -18,10 +21,33 @@ def get_all_mutation_sources(trees, tp=None):
     for tree in trees:
         visitor.visit(tree)
     conditions = visitor.conditions
+    definitions = visitor.definitions
     if tp is not None:
         conditions = [c for c in conditions if isinstance(c, tp)]
+        definitions = [c for c in definitions if isinstance(c, tp)]
 
-    return conditions
+    return conditions, definitions
+
+class OperatorVisitor(NodeVisitor):
+
+    def __init__(self):
+        self.operators = []
+
+    def visit_BinOp(self, node):
+        self.operators.append(node)
+        return super().generic_visit(node)
+
+    def visit_AugAssign(self, node):
+        return self.visit_BinOp(node)
+
+class NameVisitor(NodeVisitor):
+
+    def __init__(self):
+        self.names = []
+
+    # TODO: group all names in the same context, so we can substitute each of
+    # them or just one at a time
+
 
 class AllMightyVisitor(NodeVisitor):
     def __init__(self):
@@ -29,8 +55,6 @@ class AllMightyVisitor(NodeVisitor):
         self.conditions_seen = set()
         self.definitions = []
         self.definitions_seen = set()
-        self.names = []
-        self.names_seen = set()
         super().__init__()
 
     def add_to(self,target_str, node, attr):
@@ -44,8 +68,8 @@ class AllMightyVisitor(NodeVisitor):
         for elem in elems:
             elem_str = astor.to_source(elem)
             if elem_str not in target_seen:
-                self.conditions.append(elem)
-                self.conditions_seen.add(elem_str)
+                target.append(elem)
+                target_seen.add(elem_str)
 
     def visit_FunctionDef(self, node):
         self.add_to('definitions', node, 'name')
@@ -54,12 +78,16 @@ class AllMightyVisitor(NodeVisitor):
     def visit_AsyncFunctionDef(self, node):
         self.visit_FunctionDef(node)
 
-    def visit_Class(self, node):
+    def visit_ClassDef(self, node):
         self.visit_FunctionDef(node)
 
     def visit_BoolOp(self, node):
         self.add_to('conditions', node, 'values')
         return super().generic_visit(node)
+
+    def visit_Assign(self, node):
+        for target in node.targets:
+            self.add_to('definitions', target, 'name')
 
     def visit_UnaryOp(self, node):
         if isinstance(node.op, ast.Not):
@@ -77,17 +105,15 @@ class AllMightySuperMutator(ConditionMutator):
     def __init__(self, *args, **kwargs):
         """Constructor. Arguments are as with `StatementMutator` constructor."""
         super().__init__(*args, **kwargs)
-        self.rand = Random()
-        self.rand.seed(42)
-        self.conditions = get_all_mutation_sources(self.source)
-        if self.log:
-            print("Found conditions",
-                  [astor.to_source(cond).strip() 
-                   for cond in self.conditions])
+        self.conditions, self.definitions = get_all_mutation_sources(self.source)
 
     def choose_condition(self):
         """Return a random condition from source."""
         return copy.deepcopy(random.choice(self.conditions))
+
+    def choose_name(self):
+        """ Return a random name from source. """
+        return copy.deepcopy(random.choice(self.names))
 
     def choose_bool_op(self):
         return random.choice(['set', 'not', 'and', 'or'])
@@ -95,7 +121,7 @@ class AllMightySuperMutator(ConditionMutator):
     def swap(self, node):
         # i dont know why, but i need another random instance or things stop
         # working
-        swap_func_name = "swap_" + self.rand.choice(['condition'])
+        swap_func_name = "swap_" + rand.choice(['condition', 'operator' ])
         swap_func = getattr(self, swap_func_name)
         return swap_func(node)
         #  return self.swap_condition(node)
@@ -104,7 +130,26 @@ class AllMightySuperMutator(ConditionMutator):
         return ConditionMutator.swap(self, node)
 
     def swap_name(self, node):
-        raise Exception("TODO: Implement")
+        node = deepcopy(node)
+        visitor = NameVisitor()
+        visitor.visit(node)
+        names = visitor.names
+        if not names:
+            return super().swap(node)
+        # choose a random name from definitions to swap
+        target = rand.choice(names)
+        target.name = rand.choice(self.definitions)
+        return node
 
     def swap_operator(self, node):
-        raise Exception("TODO: Implement")
+        node = deepcopy(node)
+        visitor = OperatorVisitor()
+        visitor.visit(node)
+        operators = visitor.operators
+        if not operators:
+            return super().swap(node)
+        # choose a random operator to swap
+        target = rand.choice(operators)
+        operators = [ast.Add ,ast.Mult]
+        target.op = rand.choice(operators)()
+        return node
